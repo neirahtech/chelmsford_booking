272e437be5cbc031804fdd7307ba782171838a98
Anchuthan018 <147297524+Anchuthan018@users.noreply.github.com>
2025-03-26 16:38:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BookingForm from './components/bookingForm'
+
+const App = () => {
+  return (
+    <div>
+      <BookingForm />
+    </div>
+  )
+}
+
+export default App
